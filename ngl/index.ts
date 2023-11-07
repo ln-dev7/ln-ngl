147d9ad0467e06b5378ecac24a9bf38efde1fa7a
LN <leonelngoya@gmail.com>
2023-11-07 23:07:52 +0100

    first commit

---
+++ ngl/index.ts
@@ -0,0 +1,85 @@
+export const ngl = [
+  {
+    question: "Le Pro 🤧",
+    response: "L'air 😂",
+  },
+  {
+    question:
+      "Je te suit depuis. Mais tu n'est pas dispo pour être mon mentor...",
+    response: "Vraiment ",
+  },
+  {
+    question: "Bonsoir Léo. On fait comment pour coder comme toi?",
+    response: "",
+  },
+  {
+    question: "Tu es une inspiration jeune homme",
+    response: "",
+  },
+  {
+    question:
+      "J'aimerais vraiment commemcer à apprendre le dev mais je ne sais pas par où commencer et j'hésite à te dm.",
+    response: "",
+  },
+  {
+    question: "je peux être très honnête avec toi ici ?",
+    response: "",
+  },
+  {
+    question: "how many hours of sleep you get last night? ",
+    response: "",
+  },
+  {
+    question: "Go",
+    response: "GO !!!",
+  },
+  {
+    question:
+      "Est-tu parfois effrayé par l'idée qu'un jour tu te feras remplacer par une IA ",
+    response: "",
+  },
+  {
+    question:
+      "Quel est votre vision dans le monde dev? Genre dans 5 ans tu penses migrer dans d'autres secteurs où y rester ?",
+    response: "",
+  },
+  {
+    question:
+      "Comment tu fais pour que t'es clients respectent ton travail sachant que le free-lance vivant en Afrique ne sont pas tellement respecté ",
+    response: "",
+  },
+  {
+    question:
+      "Comment tu fais pour exercer ton activité de free-lance,tu proposes de services sur de plateforme ou aux alentours ?",
+    response: "",
+  },
+  {
+    question: "Tu as quitté mboa digital ou tu y travailles toujours ? ",
+    response: "",
+  },
+  {
+    question: "Coeur a prendre?😆",
+    response: "",
+  },
+  {
+    question:
+      "Pourquoi tu ne dors jamais ? Tu as 70h par jour au lieu de 24 ? Tu es même humain ? Sinon c'est super ce que tu fais, continue comme ça ",
+    response: "",
+  },
+  {
+    question: "Combien gagne un développeur front-end ?",
+    response: "",
+  },
+  {
+    question: "Tôt ou tard J'aurais ton niveau ",
+    response: "",
+  },
+  {
+    question: "je suis là pour te soutenir 💗",
+    response: "",
+  },
+  {
+    question: "t'es trop cool",
+    response: "",
+  },
+];
